9ea4239f7f4a26bcc793d2f5d6db8083153e2676
Colin Duwe <colinduwe@gmail.com>
2026-05-21 16:23:07 -0700

    rebuild assets with new most popular sort options

--- build/reset/view.asset.php
+++ build/reset/view.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('@wordpress/interactivity', array('id' => '@wordpress/interactivity-router', 'import' => 'dynamic')), 'version' => '263edb44ec0218996348', 'type' => 'module');
+<?php return array('dependencies' => array('@wordpress/interactivity', array('id' => '@wordpress/interactivity-router', 'import' => 'dynamic')), 'version' => '4e987da5f9a074669118', 'type' => 'module');
--- build/taxonomy/view.asset.php
+++ build/taxonomy/view.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('@wordpress/interactivity', array('id' => '@wordpress/interactivity-router', 'import' => 'dynamic')), 'version' => '6e7dfcb65eff18cb8303', 'type' => 'module');
+<?php return array('dependencies' => array('@wordpress/interactivity', array('id' => '@wordpress/interactivity-router', 'import' => 'dynamic')), 'version' => 'a9f48644ae388a411605', 'type' => 'module');
